6964ae951ad43693869565d034f94bedfb93b876
Marius Feteanu <mariusfeteanu@fb.com>
2022-08-14 19:54:24 +0100

    simple confg parsing

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,26 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = parse_config(&args);
+
+    println!("Will search for {} in {}.", config.query, config.file_path);
+
+    let file_contents = fs::read_to_string(config.file_path).expect("Expected file did not exist.");
+
+    println!("File contents: \n{file_contents}\n")
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
 }
