3822839217d8e946878cd1fe35cb64075829ce28
Marius Feteanu <mariusfeteanu@fb.com>
2022-08-14 20:08:29 +0100

    some safer parsin

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,16 +1,19 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::build(&args)?;
 
     println!("Will search for {} in {}.", config.query, config.file_path);
 
     let file_contents = fs::read_to_string(config.file_path).expect("Expected file did not exist.");
 
-    println!("File contents: \n{file_contents}\n")
+    println!("File contents: \n{file_contents}\n");
+
+    Ok(())
 }
 
 struct Config {
@@ -18,9 +21,14 @@ struct Config {
     file_path: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
 
-    Config { query, file_path }
+        return Ok(Config { query, file_path });
+    }
 }
