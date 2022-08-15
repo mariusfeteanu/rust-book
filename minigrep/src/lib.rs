ef0c8033e259c935afbb5521e7813d87bcf09fcb
Marius Feteanu <mariusfeteanu@fb.com>
2022-08-15 22:13:54 +0100

    use iterators in minigrep

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -17,12 +17,9 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        let query = args.next().expect("please specify a query");
+        let file_path = args.next().expect("please specify a file path");
 
         return Ok(Config { query, file_path });
     }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,9 +4,7 @@ use std::error::Error;
 use minigrep::Config;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(&args)?;
+    let config = Config::build(env::args())?;
     if let Err(e) = minigrep::run(config) {
         println!("Application error {e}!");
         return Err(e);
